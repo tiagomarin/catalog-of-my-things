a227799c14d25768b465f48d0a8dd7f1f12037bd
jdbs9514 <jdbs9514@gmail.com>
2022-10-24 16:25:50 -0500

    add method can be archive? and move to archive

--- classes/item.rb
+++ classes/item.rb
@@ -1,10 +1,11 @@
 require 'securerandom'
+require 'date'
 
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date
   attr_reader :id, :archived
 
-  def initialize(date)
+  def initialize(date, archived: false)
     @id = SecureRandom.uuid
     @genre = genre
     @author = author
@@ -12,5 +13,19 @@ class Item
     @label = label
     @publish_date = date
     @archived = archived
+  end
+
+  def can_be_archived?
+    now = Date.today
+    date_array = @publish_date.split(/-/)
+    p_date = Date.civil(date_array[0].to_i, date_array[1].to_i, date_array[2].to_i)
+    difference_in_days = (now - p_date).to_i
+    years = (difference_in_days/365.25).to_i
+
+    years >= 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 end
