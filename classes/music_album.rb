63b7f4d4253504fa12bb22b6f896b2c5b5ff06a7
CindyDorantes <cindym.dorantes@gmail.com>
2022-10-26 12:26:11 -0500

    create Genre class and add_item method

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -2,6 +2,8 @@ require_relative './item'
 require 'securerandom'
 
 class MusicAlbum < Item
+  attr_accessor :on_spotify
+
   def initialize(title, date, on_spotify, id = SecureRandom.random_number(1000))
     super(title, date, id)
     @on_spotify = on_spotify
--- test.rb
+++ test.rb
@@ -1,7 +1,10 @@
 require_relative './classes/music_album'
+require_relative './classes/genre'
 require 'pry'
 
 album01 = MusicAlbum.new('Music', '2000-09-18', true)
 album02 = MusicAlbum.new('Motomami', '2022-03-18', true)
+genre01 = Genre.new('Alternative')
+genre02 = Genre.new('Pop')
 binding.pry
 p album01
