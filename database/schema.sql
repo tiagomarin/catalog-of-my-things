1e2d69a6d62b70e720b7bcee4344911808e2a4be
Tiago Marin <tiago.lelinski@gmail.com>
2022-10-26 11:40:41 -0300

    add sources and movie tables in schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE sources(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE movies(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN,
+    silent BOOLEAN,
+    sources_ids INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_sources FOREIGN KEY(sources_id) REFERENCES sources(id)
+);
