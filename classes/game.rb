47a3cee5ced636d416d209076d4df40c4ddb6f9e
jdbs9514 <jdbs9514@gmail.com>
2022-10-27 12:23:53 -0500

    complete the methods of the game and author calsses

--- classes/game.rb
+++ classes/game.rb
@@ -6,16 +6,23 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
   attr_reader :id
 
-  def initialize(title, multiplayer, last_played_at)
-    super(id = SecureRandom.random_number(1000), title, publish_date)
-    @id = id
-    @title = title
+  def initialize(title, publish_date, multiplayer, last_played_at, id = SecureRandom.random_number(1000))
+    super(title, publish_date, id)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
+  def two_years_old?
+    now = Date.today
+    date_array = @last_played_at.split(/-/)
+    p_date = Date.civil(date_array[0].to_i, date_array[1].to_i, date_array[2].to_i)
+    difference_in_days = (now - p_date).to_i
+    years = (difference_in_days / 365.25).to_i
+
+    years >= 2
+  end
+
   def can_be_archived?
-    super && (Date.today.year - @last_played_at.year) > 2
+    two_years_old? && super
   end
 end
-# id = SecureRandom.random_number(1000), 
--- test1.rb
+++ test1.rb
@@ -1,11 +1,12 @@
 require_relative './classes/game'
 require_relative './classes/author'
 
-game1 = Game.new('dota', 'multiplayer', '2020-10-26')
-game2 = Game.new('Lol', 'multiplayer', '2022-10-26')
-p game1
-p game2
+game1 = Game.new('dota', '2010-10-05', true, '2020-10-26')
+game2 = Game.new('Lol', '2015-10-05', true, '2022-10-26')
+p game1.can_be_archived?
+p game2.can_be_archived?
 author1 = Author.new('Erick', 'Johnson')
 author2 = Author.new('Tom', 'Cadwell')
-p author1
-p author2
+p author1.add_item(game1)
+p author2.add_item(game2)
+p author1.items
